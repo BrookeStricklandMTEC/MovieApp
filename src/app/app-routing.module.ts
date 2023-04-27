83d510bde230208d522e03f30a28163605234f8a
Brooke Strickland <brooke.strickland2904@stu.mtec.edu>
2023-04-27 14:46:38 -0600

    starter

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MovieHomeComponent } from './components/movie-home/movie-home.component';
+import { FavoriteMovieComponent } from './components/favorite-movie/favorite-movie.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // path '' : login / register? 
+  { path: '', component: MovieHomeComponent },
+  { path: 'favoriteMovie', component: FavoriteMovieComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar/nav-bar.component';
+import { FavoriteMovieComponent } from './components/favorite-movie/favorite-movie.component';
+import { MovieHomeComponent } from './components/movie-home/movie-home.component';
+import { MoviesComponent } from './components/movies/movies.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    FavoriteMovieComponent,
+    MovieHomeComponent,
+    MoviesComponent
   ],
   imports: [
     BrowserModule,
