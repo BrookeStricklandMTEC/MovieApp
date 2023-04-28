322cdfca6e7670588f7c21fdc77bd7165fc687ae
Brooke Strickland <brooke.strickland2904@stu.mtec.edu>
2023-04-28 08:50:32 -0600

    pulling enviornment

--- src/app/service/movie.service.ts
+++ src/app/service/movie.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
 
+// Brookes Imports 
+  import { HttpClient } from '@angular/common/http';
+  // import {enviornment}
+  import { Observable, map } from 'rxjs';
+  import { Movie } from '../interfaces/movie';
+// 
+
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  browseMovies(searchText: string): Observable<Movie[]>{
+    const alteredText = searchText.replace(/\s/g, '+');
+    return this.http.get<Movie[]>(``).pipe(
+        map((response : any) => response['games']
+      )
+    )
+  }
 }
